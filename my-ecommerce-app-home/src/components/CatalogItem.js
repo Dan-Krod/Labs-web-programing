d3b9e0624a49556e7c95537715ed9e7042f9ce41
Dan Krod <dan.ga17da@gmail.com>
2024-11-10 10:54:55 +0200

    Update CatalogItem.js

--- my-ecommerce-app-home/src/components/CatalogItem.js
+++ my-ecommerce-app-home/src/components/CatalogItem.js
@@ -1,3 +1,4 @@
+// CatalogItem.js
 import React from 'react';
 import PrimaryButton from './PrimaryButton';
 import '../styles/CatalogItem.css';
@@ -6,12 +7,20 @@ const CatalogItem = ({ product, onViewDetails }) => {
   return (
     <div className="catalog-item">
       <div className="catalog-item-category">{product.category}</div>
-      <img src={product.image} alt={product.name} className="catalog-item-image" />
+      <img 
+        src={product.image_url} 
+        alt={product.name} 
+        className="catalog-item-image" 
+      />
       <h2>{product.name}</h2>
       <p className="catalog-item-description">{product.description}</p>
       <p className="catalog-item-additional">{product.additionalInfo}</p>
       <p className="catalog-item-price">Price: ${product.price}</p>
-      <PrimaryButton className="primary-button" label="View more" onClick={() => onViewDetails(product)} />
+      <PrimaryButton 
+        className="primary-button" 
+        label="View more" 
+        onClick={() => onViewDetails(product)} 
+      />
     </div>
   );
 };
