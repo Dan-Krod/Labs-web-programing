53ce197ff9a9e5c1fad649cab310986e2455572e
Dan Krod <dan.ga17da@gmail.com>
2024-11-22 11:48:19 +0200

    Update server.js

--- my-ecommerce-app-home/server.js
+++ my-ecommerce-app-home/server.js
@@ -1,8 +1,8 @@
+// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const bookRouter = require('./route'); 
 const db = require('./database'); 
 
 const app = express();
@@ -10,19 +10,15 @@ const PORT = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/images', express.static(path.join(__dirname, 'public')));
-
-app.use('/api', bookRouter);
-
-app.get('/', (req, res) => {
-    res.send('API is running');
-});
+app.use('/api', require('./routes'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Сервер працює на http://localhost:${PORT}`);
 });
