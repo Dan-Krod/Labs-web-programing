67760fdd3c61ccedd9b4d5388ad2b38ee08e079f
Dan Krod <dan.ga17da@gmail.com>
2024-11-22 11:46:52 +0200

    Update index.js

--- my-ecommerce-app-home/src/index.js
+++ my-ecommerce-app-home/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+// src/index.js
+import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'; // Імпортуємо Provider з react-redux
+import store from './redux/store'; // Імпортуємо store з файлу store.js
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}> {/* Додаємо Provider і передаємо store */}
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
